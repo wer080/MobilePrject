e6032e3afc77c4e008c227b0f35a51f0e76910c6
wer080 <wer080@ajou.ac.kr>
2020-05-18 17:44:13 +0900

    Android Project # first commit # Just get data from Firestore and show it on create # using call back method

---
+++ app/src/main/java/com/example/mpproject/GetData.java
@@ -0,0 +1,122 @@
+package com.example.mpproject;
+
+import android.util.Log;
+import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class InFieldData{
+    private List<String> examinDate = new ArrayList<String>();
+    private List<String> grade = new ArrayList<String>();
+    private List<Double> price = new ArrayList<Double>();
+    private List<String> product_name = new ArrayList<String>();
+    private List<String> species = new ArrayList<String>();
+    private List<String> unit = new ArrayList<String>();
+    private List<Double> changeRate = new ArrayList<Double>();
+
+    public void SetExaminDate(String val){
+        examinDate.add(val);
+    }
+    public void SetGrade(String val){
+        grade.add(val);
+    }
+    public void SetProductName(String val){
+        product_name.add(val);
+    }
+    public void SetSpecies(String val){
+        species.add(val);
+    }
+    public void SetUnit(String val){
+        unit.add(val);
+    }
+    public void SetPrice(double val){
+        price.add(val);
+    }
+    public void SetChangeRate(double val){
+        changeRate.add(val);
+    }
+
+    public List<String> GetExaminDate(){
+        return examinDate;
+    }
+
+    public List<String> GetGrade(){
+        return grade;
+    }
+    public List<String> GetProductName(){
+        return product_name;
+    }
+    public List<String> GetSpecies(){
+        return species;
+    }
+    public List<String> GetUnit(){
+        return unit;
+    }
+    public List<Double> GetPrice(){
+        return price;
+    }
+    public List<Double> GetChangeRate(){
+        return changeRate;
+    }
+
+}
+
+public class GetData{
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private ReturnData rd;
+    public interface ReturnData {
+        void receiveData(InFieldData d);
+    }
+
+    public void SetOnCB(ReturnData callback){
+        rd = callback;
+    }
+
+
+    public void GetMainItem(int option) {
+        if (option == 0) {
+            DocumentReference docRef = db.collection("best_item_yesterday").document("yd_best");
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            List list = (List) document.getData().get("data");
+                            InFieldData data = new InFieldData();
+                            for (int i = 0; i < list.size(); i++) {
+                                HashMap inData = (HashMap) list.get(i);
+                                data.SetExaminDate(inData.get("examin_date").toString());
+                                data.SetChangeRate((double) inData.get("vsYD"));
+                                data.SetGrade(inData.get("grade").toString());
+                                data.SetPrice((double) inData.get("price"));
+                                data.SetProductName(inData.get("product_name").toString());
+                                data.SetSpecies(inData.get("species").toString());
+                                data.SetUnit(inData.get("unit").toString());
+                            }
+                            rd.receiveData(data);
+                        } else {
+                            Log.d("Check", "No such document");
+                        }
+                    } else {
+                        Log.d("Check", "get failed with ", task.getException());
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mpproject/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.mpproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity{
+    GetData db = new GetData();
+    InFieldData getdata = new InFieldData();
+
+    public void SetDataFile(InFieldData d){
+        this.getdata = d;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GetData.ReturnData callback = new GetData.ReturnData() {
+            @Override
+            public void receiveData(InFieldData d) {
+                SetDataFile(d);
+                FilledData(d);
+            }
+        };
+        db.SetOnCB(callback);
+        db.GetMainItem(0);
+
+    }
+
+    void FilledData(InFieldData getdata){
+        List<String> product_name = new ArrayList<String>();
+        List<String> product_species = new ArrayList<String>();
+        List<String> product_grade = new ArrayList<String>();
+        List<String> product_unit = new ArrayList<String>();
+        List<Double> product_price = new ArrayList<Double>();
+        List<Double> product_vsYD = new ArrayList<Double>();
+        List<String> product_examindate = new ArrayList<String>();
+
+        product_name = getdata.GetProductName();
+        product_species = getdata.GetSpecies();
+        product_grade = getdata.GetGrade();
+        product_unit = getdata.GetUnit();
+        product_price = getdata.GetPrice();
+        product_vsYD = getdata.GetChangeRate();
+        product_examindate = getdata.GetExaminDate();
+
+        for(int i = 0; i < 2; i++) {
+            String textSID = "data_name_" + (i + 1);
+            int textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            TextView text = (TextView) findViewById(textID);
+            text.setText(product_name.get(i));
+
+            textSID = "data_species_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_species.get(i));
+
+            textSID = "data_grade_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_grade.get(i));
+
+            textSID = "data_unit_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_unit.get(i));
+
+            textSID = "data_price_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_price.get(i).toString());
+
+            textSID = "data_vsYD_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_vsYD.get(i).toString());
+
+            textSID = "data_examindate_" + (i + 1);
+            textID = getResources().getIdentifier(textSID, "id", getPackageName());
+            text = (TextView) findViewById(textID);
+            text.setText(product_examindate.get(i));
+        }
+    }
+}
